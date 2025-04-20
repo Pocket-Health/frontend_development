c4910a0849a70493d1e8f4ffc4ce6753bf97cd5d
nmasalkin <nmasalkin96@yandex.ru>
2025-04-20 05:51:06 +0300

    The main part of the entrance screen is made

--- lib/feature/load/view/load_screen.dart
+++ lib/feature/load/view/load_screen.dart
@@ -1,121 +1,91 @@
-import 'dart:math' as math;
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class LoadScreen extends StatefulWidget {
-  const LoadScreen({Key? key}) : super(key: key);
+  const LoadScreen({super.key});
 
   @override
   _LoadScreenState createState() => _LoadScreenState();
 }
 
-class _LoadScreenState extends State<LoadScreen> with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _rotation;
-
+class _LoadScreenState extends State<LoadScreen> {
   @override
   void initState() {
     super.initState();
-
-    _controller = AnimationController(
-      duration: const Duration(seconds: 2),
-      vsync: this,
-    );
-
-    _rotation = Tween<double>(begin: 0, end: 2 * math.pi).animate(CurvedAnimation(
-      parent: _controller,
-      curve: Curves.easeInOut,
-    ));
-
-    _startRotationLoop();
-  }
-
-  void _startRotationLoop() async {
-    while (mounted) {
-      await _controller.forward();
-      await Future.delayed(const Duration(seconds: 2));
-      _controller.reset();
-    }
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+    Future.delayed(const Duration(seconds: 3), () {
+      if (mounted) {
+        Navigator.of(context).pushReplacementNamed('/login');
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned(
-            top: 0,
-            right: 0,
-            child: SvgPicture.asset(
-              'assets/images/load_screen_up.svg',
-              width: screenSize.width,
-            ),
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            child: SvgPicture.asset(
-              'assets/images/load_screen_down.svg',
-              width: screenSize.width - 40,
-            ),
-          ),
-          Center(
-            child: AnimatedBuilder(
-              animation: _rotation,
-              builder: (context, child) {
-                return Transform.rotate(
-                  angle: _rotation.value,
-                  child: child,
-                );
-              },
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "POCKET",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 75,
-                      fontFamily: 'Poppins',
-                      fontWeight: FontWeight.w800,
-                      height: 0.7,
-                      shadows: [
-                        Shadow(
-                          color: Colors.black45,
-                          offset: Offset.zero,
-                          blurRadius: 30,
-                        ),
-                      ],
-                    ),
-                  ),
-                  Text(
-                    "HEALTH",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 75,
-                      fontFamily: 'Poppins',
-                      fontWeight: FontWeight.w800,
-                      shadows: [
-                        Shadow(
-                          color: Colors.black45,
-                          offset: Offset.zero,
-                          blurRadius: 30,
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+      body: Builder(
+        builder: (context) {
+          return Stack(
+            children: [
+              Positioned(
+                top: 0,
+                right: 0,
+                child: SvgPicture.asset(
+                  'assets/images/load_screen_up.svg',
+                  width: screenSize.width,
+                ),
               ),
-            ),
-          ),
-        ],
+              Positioned(
+                bottom: 0,
+                left: 0,
+                child: SvgPicture.asset(
+                  'assets/images/load_screen_down.svg',
+                  width: screenSize.width - 40,
+                ),
+              ),
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "POCKET",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 75,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w800,
+                        height: 0.7,
+                        shadows: [
+                          Shadow(
+                            color: Colors.black45,
+                            offset: Offset.zero,
+                            blurRadius: 30,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Text(
+                      "HEALTH",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 75,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w800,
+                        shadows: [
+                          Shadow(
+                            color: Colors.black45,
+                            offset: Offset.zero,
+                            blurRadius: 30,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -3,4 +3,30 @@ import 'package:flutter/material.dart';
 final theme = ThemeData(
   scaffoldBackgroundColor: Color.fromARGB(255, 36, 44, 59),
   primarySwatch: Colors.blue,
+  textTheme: TextTheme(
+    titleMedium: TextStyle(
+      color: Colors.white,
+      fontSize: 35,
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.bold,
+    ),
+    titleSmall: TextStyle(
+      color: Colors.white,
+      fontSize: 20,
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.bold,
+    ),
+    labelMedium: TextStyle(
+      color: Color.fromARGB(200, 36, 44, 59),
+      fontSize: 25,
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.bold,
+    ),
+    labelSmall: TextStyle(
+      color: Color.fromARGB(200, 36, 44, 59),
+      fontSize: 20,
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.bold,
+    ),
+  ),
 );
